12f376f2b49a5b3c7e8344152adee52444a7a828
CS17B011 <cs17b011@iittp.ac.in>
2020-04-09 19:42:07 +0530

    Account Head Delete

--- controllers/account_head.js
+++ controllers/account_head.js
@@ -55,27 +55,49 @@ router.post('/', (req, res) => {
 
 router.post('/edit', (req, res) => {
     getConnection((err, connection) => {
-        if (err)
-        {
-            console.log(err);    
+        if (err) {
+            console.log(err);
         }
-        else
-        {
+        else {
             var { account_id, account_name, account_type, is_society, village_id } = req.body;
             var sql = " UPDATE `Account_Head` SET `account_name` = ?, `account_type` = ?, `is_society` = ?, `village_id` = ? WHERE `Account_Head`.`account_id` = ? ";
             connection.query(sql, [account_name, account_type, is_society, village_id, account_id], (err, results) => {
                 connection.release();
-                if (err)
-                {
+                if (err) {
                     console.log(err);
                 }
-                else
-                {
+                else {
                     res.redirect('/accounthead');
                 }
             });
         }
     })
+});
+
+router.post('/delete', (req, res) => {
+    console.log(req.body.ids);
+    getConnection((err, connection) => {
+        if (err)
+        {
+            console.log(err);    
+        }    
+        else
+        {
+            var sql = "DELETE FROM `Account_Head` WHERE account_id IN (?)";
+            connection.query(sql, [req.body.ids], (err, results) => {
+                connection.release();
+                if (err)
+                {
+                    console.log(err);    
+                }
+                else
+                {
+                    console.log(results);
+                    res.redirect('/accounthead');
+                }
+            });
+        }
+    });
 })
 
 module.exports = router;
